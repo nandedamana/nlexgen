9e9eb35a840ac15af6cc13b37822dcd4d49f0236
Nandakumar Edamana <nandakumar@nandakumar.co.in>
2019-07-23 18:18:10 +0530

    test file updates

--- src/test1/main.c
+++ src/test1/main.c
@@ -4,6 +4,10 @@
 
 #include "read.h"
 
+#define NAN_TOK_NONE  0
+#define NAN_TOK_HELLO 1
+#define NAN_TOK_HI    2
+
 int main()
 {
 	fpin  = stdin;
@@ -15,11 +19,25 @@ int main()
 	 * (buf + BUFLEN) actually points to the first byte next to the end of the buffer.
 	*/
 	bufendptr = buf + BUFLEN;
-
+	
+	int token;
 	char ch;
 
-	ch = nan_getchar();
-	#include "out/lexbranch.c"
+	do {
+		token = NAN_TOK_NONE;
+
+		ch = nan_getchar();
+		#include "out/lexbranch.c"
+		
+		if(token == NAN_TOK_NONE) {
+			fprintf(stderr, "ERROR: Unknown token.\n");
+			exit(1);
+		}
+		else {
+			if(ch == ' ' || ch == '\n' || ch == 0 || ch == EOF) /* Separator */
+				fprintf(fpout, "Token detected: %d\n", token);
+		}
+	} while(ch && ch != EOF);
 
 	return 0;
 }
