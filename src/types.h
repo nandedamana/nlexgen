f91ee14b3fe1d8a4337c6bdfeeff52c82fd183a7
Nandakumar Edamana <nandakumar@nandakumar.co.in>
2021-05-19 16:08:37 +0530

    types.h regenerated with the latest version of ngg

--- src/types.h
+++ src/types.h
@@ -4,37 +4,40 @@
  typedef int NlexCharacter;
 
 typedef enum NlexErr {
-NLEX_ERR_NONE = 0,
-NLEX_ERR_MALLOC,
-NLEX_ERR_REALLOC,
-NLEX_ERR_READING,
+	NLEX_ERR_NONE = 0,
+	NLEX_ERR_MALLOC,
+	NLEX_ERR_REALLOC,
+	NLEX_ERR_READING,
 } NlexErr;
 
  typedef unsigned int NanTreeNodeId;
 
 typedef struct NlexHandle {
-size_t buf_alloc_unit;
-void *(* on_error)();
-void *(* on_consume)();
-void * userdata;
-FILE * fp;
-char * buf;
-char * bufptr;
-char * bufendptr;
-size_t lastmatchat;
-_Bool eof_read;
-char * auxbuf;
-char * auxbufptr;
-char * auxbufend;
-unsigned int curstate;
-unsigned int last_accepted_state;
-unsigned int * tstack ;
-size_t tstack_top;
-unsigned int * nstack ;
-size_t nstack_top;
-_Bool done;
-void * data;
+	size_t buf_alloc_unit;
+	void (* on_error)(struct NlexHandle * nh , NlexErr errno );
+	void (* on_consume)(struct NlexHandle * nh );
+	void * userdata;
+	FILE * fp;
+	char * buf;
+	char * bufptr;
+	char * bufendptr;
+	size_t lastmatchat;
+	_Bool eof_read;
+	char * auxbuf;
+	char * auxbufptr;
+	char * auxbufend;
+	unsigned int curstate;
+	unsigned int last_accepted_state;
+	unsigned int * tstack ;
+	size_t tstack_top;
+	unsigned int * nstack ;
+	size_t nstack_top;
+	_Bool done;
+	void * data;
 } NlexHandle;
 
+static inline void NlexHandle_construct(NlexHandle * this)
+{
+}
 
 #endif /* _N96E_LEX_TYPES_H */
