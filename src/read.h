1369a2a7e2e8bcd11b1403d6917a4535baec69d2
Nandakumar Edamana <nandakumar@nandakumar.co.in>
2021-06-02 13:12:35 +0530

    moved nlex_next() to the header file to make compiler optimization easier

--- src/read.h
+++ src/read.h
@@ -250,7 +250,70 @@ static inline void nlex_nstack_remove(NlexHandle * nh, NanTreeNodeId id)
 			nh->nstack[i] = 0; // TODO better if I can remove
 }
 
-int nlex_next(NlexHandle * nh);
+/* Behaviour:
+ * On EOF, sets nh->eof_read to 1, appends the buf with nullchar, and returns EOF.
+ * Subsequent calls to nlex_last(nh) will return 0 and nlex_next() will return EOF.
+ * on_next callback is not called for EOF (even for the first time).
+ */
+static inline int nlex_next(NlexHandle * nh)
+{
+	_Bool  eof_read = 0;
+	size_t bytes_read;
+
+	if(nh->eof_read)
+		return EOF;
+
+	nh->bufptr++;
+
+	/* If fp is NULL, bufptr is assumed to be pointed to a pre-filled buffer.
+	 * This helps tokenize strings directly.
+	 */
+	if(nh->fp && (nh->bufptr == nh->bufendptr)) {
+		/* This is the best place to check */
+		if(feof(nh->fp)) {
+			eof_read   = 1;
+			bytes_read = 1; /* To append nullchar */
+		}
+
+		/* We have to read more */
+		size_t curlen = nh->bufendptr - nh->buf;
+		nh->buf       = nlex_realloc(nh, nh->buf, curlen + nh->buf_alloc_unit);
+
+		/* Because realloc() might have relocated the buffer */
+		nh->bufptr    = nh->buf + curlen;
+
+		if(!eof_read) {
+			bytes_read = fread(nh->bufptr, 1, nh->buf_alloc_unit, nh->fp);
+			if(ferror(nh->fp))
+				nh->on_error(nh, NLEX_ERR_READING);
+		}
+
+		/* Really important. The feof() chech at the top fails to detect the end if the file size is a multiple of buf_alloc_unit and the previous read had consumed the last block, leaving nothing for this call to read. */
+		if(bytes_read == 0) {
+			eof_read   = 1;
+			bytes_read = 1; /* To fill with nullchar */
+		}
+
+		/* Really important */
+		if(bytes_read < nh->buf_alloc_unit) {
+			nh->buf    = nlex_realloc(nh, nh->buf, curlen + bytes_read);
+			nh->bufptr = nh->buf + curlen;
+		}
+		
+		/* Points to the memory location next to the last character. */
+		nh->bufendptr = nh->bufptr + bytes_read;
+
+
+		if(eof_read) {
+			*(nh->bufptr) = '\0';
+			nh->eof_read  = 1;
+			return EOF;
+		}
+	}
+
+	/* Now return the character */
+	return *(nh->bufptr);
+}
 
 void nlex_onerror(NlexHandle * nh, int errno);
 
