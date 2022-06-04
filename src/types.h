fe300c747af9f549fed1a95b5206550c6c6c728b
Nandakumar Edamana <nandakumar@nandakumar.co.in>
2022-06-04 19:40:17 +0530

    change a parameter name from errno to err_no because it conflicts with the libc function

--- src/types.h
+++ src/types.h
@@ -17,8 +17,8 @@ typedef struct NlexNString {
 typedef unsigned int NanTreeNodeId;
 typedef struct NlexHandle {
 	size_t buf_alloc_unit;
-	void (* on_error)(struct NlexHandle * nh , NlexErr errno );
-	void (* on_consume)(struct NlexHandle * nh , size_t offset, size_t len);
+	void (*on_error)(struct NlexHandle *nh, NlexErr err);
+	void (*on_consume)(struct NlexHandle *nh, size_t offset, size_t len);
 	void * userdata;
 	FILE * fp;
 	char * buf;
@@ -29,16 +29,16 @@ typedef struct NlexHandle {
 	_Bool eof_read;
 	unsigned int curstate;
 	unsigned int last_accepted_state;
-	unsigned int * tstack ;
+	unsigned int *tstack;
 	size_t tstack_top;
 	size_t tstack_allocsiz;
-	unsigned int * nstack ;
+	unsigned int *nstack;
 	size_t nstack_top;
 	size_t nstack_allocsiz;
 } NlexHandle;
 
-static inline void NlexHandle_construct(NlexHandle * this)
-{
-}
+void nlex_handle_construct(NlexHandle *this);
+void nlex_handle_destruct(NlexHandle *this);
+NlexNString nlex_n_string_default();
 
 #endif /* _N96E_LEX_TYPES_H */
