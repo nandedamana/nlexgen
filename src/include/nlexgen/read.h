2bc3faf77e7e375da9133a7262935dba17b63c45
Nandakumar Edamana <nandakumar@nandakumar.co.in>
2026-02-20 17:05:57 +0530

    commit missing file
    include/nlexgen/read.h

---
+++ src/include/nlexgen/read.h
@@ -0,0 +1,5 @@
+// This wrapper file placed inside include/nlexgen helps other programs include
+// it using `#include <nlexgen/read.h>`, avoiding confusion with read.h
+// of the including program itself (if any) and vice-versa.
+
+#include "../../read.h"
